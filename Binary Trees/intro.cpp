8356da4d6e28e3ee658039e88256961b1b67c003
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-02-16 17:22:56 +0530

    Binary Tree class

---
+++ Binary Trees/intro.cpp
@@ -0,0 +1,17 @@
+temple<typename T>
+clss BinaryTreeNode{
+    public:
+    T data;
+    BinaryTreeNode*left;
+    BinaryTreeNode*right;
+
+    BinaryTreeNode(T data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode(){
+        delete left;
+        delete right;
+    }
+};
