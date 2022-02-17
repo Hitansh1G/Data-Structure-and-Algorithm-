11fdb9e33ab581c21e3e5289127aae7bb0967a47
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-02-17 17:27:12 +0530

    sum of nodes

---
+++ Binary Trees/Sum Of Nodes.cpp
@@ -0,0 +1,27 @@
+/**********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+int getSum(BinaryTreeNode<int>* root) {
+    // Write your code here
+    if(root==nullptr) return 0;
+    int result = root->data; // root node
+    result += getSum(root->left);
+    result += getSum(root->right);
+    return result;
+}
