d19b20fc6b2544bbc61a6cb651ed04d7d9df7428
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-02-18 18:36:42 +0530

    Print Elements in Range

---
+++ BST/Print Elements in Range.cpp
@@ -0,0 +1,28 @@
+/**********************************************************
+
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+
+void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
+	// Write your code here
+    if(root==nullptr) return;
+    elementsInRangeK1K2(root->left, k1, k2);
+    if(k1<=root->data && root->data<=k2)
+        cout<< root->data << ' ';
+    elementsInRangeK1K2(root->right, k1, k2);
+}
