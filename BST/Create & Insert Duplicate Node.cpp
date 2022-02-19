7b3b7d68e3f2171195f2aa7fb0bfa253a3802725
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-02-19 12:00:21 +0530

    Create & Insert Duplicate Node

---
+++ BST/Create & Insert Duplicate Node.cpp
@@ -0,0 +1,31 @@
+/************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data)
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+void insertDuplicateNode(BinaryTreeNode<int> *root) {
+    // Write your code here
+    if(root==nullptr) return;
+    BinaryTreeNode<int>* node = new BinaryTreeNode<int>(root->data);
+    node->left = root->left;
+    root->left = node;
+    insertDuplicateNode(node->left);
+    insertDuplicateNode(root->right);
+}
