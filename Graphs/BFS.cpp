fe04ea87bc191f1dfbf6985b3ce4157398380710
Hitansh1G <hitanshgupta007@gmail.com>
2022-10-01 13:03:34 +0530

    BFS

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,12 @@
+queue<int>q;
+q.push(node)
+while(q.empty()==false){
+    int node = q.top()
+    q.pop();
+    visited[node]=1;
+    for(int i->0 to adj[node].size()){
+        if(!visited[]){
+            q.push()
+        }
+    }
+}
